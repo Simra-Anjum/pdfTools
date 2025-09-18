0d0aa0f66b1682be4dce14e1b6f6bd5f5a68414b
Simra-Anjum <simraanjum68@gmail.com>
2025-09-18 16:13:42 +0530

    Update merge.mjs

--- merge.mjs
+++ merge.mjs
@@ -1,14 +1,21 @@
 import PDFMerger from 'pdf-merger-js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const merger = new PDFMerger();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const mergepdfs = async (p1, p2) => {
+  const merger = new PDFMerger();
+
   await merger.add(p1);
   await merger.add(p2);
- 
-  let d = new Date().getTime()
-  await merger.save(`public/${d}.pdf`); 
-  return d
 
-}
-export{ mergepdfs };
+  let d = new Date().getTime();
+  const outputPath = path.join(__dirname, "public", `${d}.pdf`);
+
+  await merger.save(outputPath);
+  return d;
+};
+
+export { mergepdfs };
