9a9b0fd5d69e3c373c0f9dc83ce2e80d70554e92
Simra-Anjum <simraanjum68@gmail.com>
2025-09-18 16:14:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
-const express = require('express')
-const path = require('path')
-const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
-const {mergepdfs} =require('./merge.mjs')
-const app = express()
-const port = 3000
+const express = require('express');
+const path = require('path');
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/' });
+const { mergepdfs } = require('./merge.mjs');
 
-app.use('/static', express.static('public'))
+const app = express();
+const port = process.env.PORT || 3000;
 
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, "templates/index.html"))
-})
-app.post('/merge', upload.array('pdfs', 2), async (req, res, next) => {
- console.log(req.files)
-let d = await mergepdfs(path.join(__dirname, req.files[0].path), path.join(__dirname, req.files[1].path))
- res.redirect(`/static/${d}.pdf`); /
-//  res.send({data: req.files})
-})
+  res.sendFile(path.join(__dirname, "templates/index.html"));
+});
+
+app.post('/merge', upload.array('pdfs', 2), async (req, res) => {
+  let d = await mergepdfs(
+    path.join(__dirname, req.files[0].path),
+    path.join(__dirname, req.files[1].path)
+  );
+  res.redirect(`/static/${d}.pdf`);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
