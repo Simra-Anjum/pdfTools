5fc24c0f9472d23eda4a931c03b0df69d1e82a53
Simra-Anjum <simraanjum68@gmail.com>
2025-09-18 15:35:06 +0530

    first-commit

---
+++ merge.mjs
@@ -0,0 +1,14 @@
+import PDFMerger from 'pdf-merger-js';
+
+const merger = new PDFMerger();
+
+const mergepdfs = async (p1, p2) => {
+  await merger.add(p1);
+  await merger.add(p2);
+ 
+  let d = new Date().getTime()
+  await merger.save(`public/${d}.pdf`); 
+  return d
+
+}
+export{ mergepdfs };
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const path = require('path')
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+const {mergepdfs} =require('./merge.mjs')
+const app = express()
+const port = 3000
+
+app.use('/static', express.static('public'))
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "templates/index.html"))
+})
+app.post('/merge', upload.array('pdfs', 2), async (req, res, next) => {
+ console.log(req.files)
+let d = await mergepdfs(path.join(__dirname, req.files[0].path), path.join(__dirname, req.files[1].path))
+ res.redirect(`http://localhost:3000/static/${d}.pdf`)
+//  res.send({data: req.files})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
